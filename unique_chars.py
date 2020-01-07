92649a6decfa59108be8e40734cd0a16d1948ec8
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2020-01-07 15:52:31 +0000

    challenge

---
+++ unique_chars.py
@@ -0,0 +1,10 @@
+def uniqueChar(string): 
+    characters = {}
+    for char in string:
+        if characters.get(char, ): #if present
+            if len(string) == 0;
+            return None
+            
+            if char in characters:
+                return False
+            else:
